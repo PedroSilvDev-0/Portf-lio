072aa1bd7a48129b36ecd7eb6e330c36c14a7e5e
PedroSilvDev-0 <silvsrhyan@gmail.com>
2023-12-22 12:21:02 -0300

    Navbar em React

---
+++ src/componentes/section/Presentation.js
@@ -0,0 +1,15 @@
+import styles from './Presentation.module.css'
+
+function Presentation() {
+    return (
+        <div className={styles.presentation} id="Presentation">
+            <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
+
+            <h1>Olá, eu sou Ryan Silva!</h1>
+
+            <p>Sou um apaixonado por tecnologia e soluções inovadoras.Como Product Manager, eu tenho o compromisso de resolver problemas complexos e trazer resultados excepcionais para os negócios. Meus projetos já geraram milhões de reais em receita anualestou sempre em busca de novos desafios para superar.</p>
+        </div>
+    )
+}
+
+export default Presentation
